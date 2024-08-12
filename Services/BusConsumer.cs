22d05c84510bbc2dda2b9742db70006d923c4b0e
Pavel Gribov <pavel@gribov.pro>
2024-08-12 03:34:15 +0300

    Default constructor

--- Services/BusConsumer.cs
+++ Services/BusConsumer.cs
@@ -8,18 +8,11 @@ public interface IConsumer<T>
     Task Consume(T response);
 }
 
-public class BusConsumer : IConsumer<BusResponseModel>
+public class BusConsumer(ISynchronizationService synchronizationService) : IConsumer<BusResponseModel>
 {
-    private ISynchronizationService _synchronizationService;
-
-    public BusConsumer(ISynchronizationService sycnSynchronizationService)
-    {
-        _synchronizationService = sycnSynchronizationService;
-    }
-    
     public Task Consume(BusResponseModel response)
     {
-        ISynchronizer synchronizer = _synchronizationService.GetSynchronizer(response.Id);
+        ISynchronizer synchronizer = synchronizationService.GetSynchronizer(response.Id);
         synchronizer.SetData(response.Data);
         return Task.CompletedTask;
     }
